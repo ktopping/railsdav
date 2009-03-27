6a755b7be949b9c94d2d30fed0f65b6df79d4f1e
Matt Jankowski <mjankowski@thoughtbot.com>
2009-03-27 16:46:40 -0400

    rails 2.3 calls this Request

--- lib/railsdav/methods_patch.rb
+++ lib/railsdav/methods_patch.rb
@@ -1,5 +1,5 @@
 module ActionController
-  class AbstractRequest
+  class Request
     HTTP_METHODS = %w(get head put post delete options lock unlock propfind proppatch mkcol delete put copy move)
     HTTP_METHOD_LOOKUP = HTTP_METHODS.inject({}) { |h, m| h[m] = h[m.upcase] = m.to_sym; h }
   end
